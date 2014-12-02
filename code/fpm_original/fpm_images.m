99ef207705661e0f1287d7246d4cf051ddaaa3ba
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-12-02 13:33:12 +0530

    added code for previously taken images

---
+++ code/fpm_original/fpm_images.m
@@ -0,0 +1,102 @@
+%**************************************************************************
+% Fourier Ptychographic Imaging for transparent objects, transmitted light
+% Implementation of the original paper
+%
+% Author: Alankar Kotwal <alankarkotwal13@gmail.com>
+%
+% Make sure you run this file from the lvp-imaging directory
+%**************************************************************************
+
+% Source the config file
+
+config;
+
+%**************************************************************************
+
+% Make sure you're in the lvp-imaging directory
+
+path = pwd;
+[~, folder, ~] = fileparts(path);
+
+if(~strcmp('lvp-imaging', folder))
+    error('Run the script in the lvp-imaging directory.');
+end
+
+%**************************************************************************
+
+% Output stuff
+
+yOut = scale*yRes;
+xOut = scale*xRes;
+
+%**************************************************************************
+
+% Image acquisition stuff. Here I'm assuming a rectangular LED array
+
+images = zeros(yOut, xOut, 3, nX, nY); % Array of nX x nY 3-channel images
+kArr = zeros(2, nX, nY);               % Array of nX x nY (kX, kY)
+%filtRad = lensNA * 2*pi / ;
+
+xCen = (nX-1)*xSep/2;                  % Get center coordinates as midpoint
+yCen = (nY-1)*ySep/2;                  % of LED array. Change this.
+
+for i=1:nX
+    for j=1:nY
+        
+	% Get the images from the directory
+        images(:, :, :, i, j) = imread(tempImage, [yOut xOut]);
+        
+        % Todo: Calculate k and put it in the kArr
+        
+    end
+end
+
+%**************************************************************************
+
+% Do the actual thing
+% Algorithm in Zheng, G et al. 2013, "Wide-Field, High-Resolution Fourier
+% Ptychographic Microscopy", Nature Photonics
+
+% Initialize the output and generate an upsampled image
+imageSize = [yOut xOut];
+outputIntensity = sqrt(images(:, :, :, 1, 1));
+outputPhase = zeros(yOut, xOut, 3);
+
+while(1)  % Set a convergence criterion as RMSD between this and prev image
+    
+    % For each image we have taken
+    for i=1:nX
+        for j=1:nY
+            
+            % Reconstruct image from intensity and phase, find FFT
+            outputImage = outputIntensity .* exp(sqrt(-1)*outputPhase);
+            outputFFT = fftshift(fft2(outputImage));
+            
+            % Get our mask
+            imageMask = circularMask(imageSize, kArr(1, i, j), ...
+                                     kArr(2, i, j), filtRad);
+            
+            % Then filter around the (i,j)th k vector, get IFFT
+            tempFFT = outputFFT .* imageMask;
+            % Note: This will depend on the camera orientation. Beware
+            tempImage = ifft2(ifftshift(tempFFT));
+            
+            % Replace this magnitude by the measured magnitude
+            tempImage = sqrt(images(:, :, :, i, j)) .* ...
+                             exp(sqrt(-1)*angle(tempImage));
+            
+            % Take the FFT of this creature
+            tempFFT = fftshift(fft2(tempImage));
+            
+            % If mask is 1, replace outputFFT by tempFFT
+            outputFFT = tempFFT .* imageMask + outputFFT .*(1-imageMask);
+            
+        end
+    end
+    
+end
+
+outputImage = outputIntensity .* exp(sqrt(-1)*outputPhase);
+imwrite(outputImage, 'images/output.png');
+
+imshow(outputImage);
